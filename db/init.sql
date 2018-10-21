4900e6dc704f33990bc15318cacd8ba04305a0ff
Julius S <jluuiss@gmail.com>
2018-10-21 13:08:50 +0800

    First commit

---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE accounts (
+  username VARCHAR(64) NOT NULL,
+  email VARCHAR(230) NOT NULL,
+  contact VARCHAR(15) NOT NULL,
+  pass VARCHAR(255) NOT NULL,
+  PRIMARY KEY (username),
+);
